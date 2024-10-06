7e400498194485a5e19760f09ef266c4e922f28d
anshme <ansh.meii@gmail.com>
2024-10-06 15:23:14 +0530

    added todo

--- server/server.py
+++ server/server.py
@@ -8,3 +8,11 @@ def pinging_the_server(whoami='Anjuman'):
     current_time = datetime.now()
     logging.info(f"{whoami} has pinged at {current_time}")
     # logging.error(f"this is an error")
+
+#TODO write a function to host the server. localhost:2002
+
+
+#TODO write a function which calls pinging_the_server method -> localhost:2002/ping should be the path. How to pass a variable to /ping
+
+
+#how to call curl -v http://localhost:2002/ping
